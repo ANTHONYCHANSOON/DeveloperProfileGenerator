2c4de2a4f0e2ca14408e288683f0ff056b72ebc0
anthony soon <anthonychansoon@gmail.com>
2019-10-10 15:34:23 -0700

    fixed generatehtmjs and index.js

--- index.js
+++ index.js
@@ -1,18 +1,20 @@
 const fs = require("fs");
 const axios = require("axios");
 const inquirer = require("inquirer");
-const myrepos = [];
+const generatehtml = require("./generateHTML.js")
+//console.log(generatehtml);
+let watchers = [];
 
 inquirer.prompt([
     {
         type: "input",
-        name : "githubusername",
+        name: "githubusername",
         message: "What is your GitHub UserName?"
     },
     {
         type: "checkbox",
-        message : "color?",
-        name: "color",
+        message: "color?",
+        name: "mycolor",
         choices: [
             "green",
             "blue",
@@ -20,42 +22,63 @@ inquirer.prompt([
             "red"
         ]
     }
-]).then(function({githubusername}) {
+]).then(function ({ githubusername, mycolor }) {
+
+    console.log(githubusername);
+    console.log(mycolor);
+
     const queryURL = `https://api.github.com/users/${githubusername}/repos?per_page=100`;
+    const queryURL2 = `https://api.github.com/users/${githubusername}`;
 
-    axios.get(queryURL)
-    .then(function (res){
+    axios.get(queryURL2)
+        .then(function (res) {
 
-        console.log(res);
-        // * Profile image
-        let image = res.data[0].avatar_url;
-        // * User name
-        let username = githubusername;
-        // * Links to the following:
-        //     * User location via Google Maps
-        //     * User GitHub profile
-        //     * User blog
-        // * User bio
-        // * Number of public repositories
-        // * Number of followers
-        //  Number of GitHub stars
-        // * Number of users following
+            let githubandcolorinfo = {
+                color: mycolor,
+                profileimageurl: res.data.avatar_url,
+                username: res.data.login,
+                location: res.data.avatar_url,
+                profilelink: res.data.html_url,
+                blog: res.data.blog,
+                bio: res.data.bio,
+                publicrepos: res.data.public_repos,
+                followers: res.data.followers,
+                following: res.data.following,
+            };
+            // * Profile image
+            //console.log("profile image" + res.data.avatar_url);
+            // * User name
+            //console.log("username" + res.data.login);
+            // * Links to the following:
+            //     * User location via Google Maps
+            //console.log("location" + res.data.location);
+            //     * User GitHub profile
+            //console.log("profile link" + res.data.html_url)
+            //     * User blog
+            //console.log("blog" + res.data.blog);
+            // * User bio
+            //console.log("bio" + res.data.bio);
+            // * Number of public repositories
+            //console.log("public repos" + res.data.public_repos);
+            // * Number of followers
+            //console.log("followers" + res.data.followers);
+            // * Number of users following
+            //console.log("following" + res.data.following);
 
-        // for (let i = 0; i < res.data.length; i++)
-        // {
-        //     myrepos.push(res.data[i].name)
-        // }
-        // console.log(myrepos);
+            axios.get(queryURL)
+                .then(function (res) {
+                    for (let i = 0; i < res.data.length; i++) {
+                        watchers.push(res.data[i].watchers_count);
+                    }
+                    //console.log(watchers);
+                    let totalwatchers = watchers.reduce((a, b) => a + b);
+                    //  Number of GitHub stars
+                    //console.log("watches/stars" + totalwatchers);
 
-
-    })
-
-
-    // fs.writeFile("test.txt", JSON.stringify(data, null),function(err){
-    //     if(err){
-    //         return console.log(err);
-    //     }
-    //     console.log("success");
-    // });
-
+                    // fs.writeFile("myselection.txt",)
+                    githubandcolorinfo.stars = totalwatchers;
+                    console.log(githubandcolorinfo);
+                    generatehtml(githubandcolorinfo);
+                })
+        })
 });
